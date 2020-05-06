e96d1e9f844b688c7cd841583a1e8a3747e20e0f
barnesheaton <39313406+barnesheaton@users.noreply.github.com>
2020-05-06 13:01:28 -0600

    remove uneeded error handlers

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const Mongoose = require('mongoose')
-var createError = require('http-errors')
-var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
@@ -25,22 +23,6 @@ const Resolvers = require('./resolvers')
 
 app.use(logger('dev'))
 app.use(cookieParser())
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404))
-})
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
 
 const server = new ApolloServer({ typeDefs: Schema, resolvers: Resolvers })
 

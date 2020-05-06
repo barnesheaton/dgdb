dc2e0057ba2314962e00630bd21624006dcf9fc3
barnesheaton <39313406+barnesheaton@users.noreply.github.com>
2020-05-06 11:28:06 -0600

    pipeline stuff

--- model.js
+++ model.js
@@ -3,7 +3,8 @@ const Mongoose = require('mongoose')
 const GameSchema = Mongoose.Schema({
   name: String,
   description: String,
-  players: Number
+  players: Number,
+  new: String
 })
 
 const Game = Mongoose.model('Game', GameSchema)

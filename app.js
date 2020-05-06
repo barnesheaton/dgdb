def2bf0b353ebe2b0bdf7c19d7dfdc9d3d2327ba
barnesheaton <39313406+barnesheaton@users.noreply.github.com>
2020-05-06 12:39:39 -0600

    more node packages

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const Mongoose = require('mongoose')
+var createError = require('http-errors')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
 const PORT = 8080
 const app = express()
@@ -13,13 +17,31 @@ Mongoose.connect('mongodb://localhost/apollo', err => {
   return true
 })
 
-const seed = require('./seed')
-
+// const seed = require('./seed')
 // seed()
 
 const Schema = require('./schema')
 const Resolvers = require('./resolvers')
 // const Connectors = require('./connectors')
+
+app.use(logger('dev'))
+app.use(cookieParser())
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 const server = new ApolloServer({ typeDefs: Schema, resolvers: Resolvers })
 
